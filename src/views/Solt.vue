5f58d328b046727760d4863de52b6b469d6898b4
xiaonan39 <1294865657@qq.com>
2020-10-30 17:32:54 +0800

    master666

--- src/views/Solt.vue
+++ src/views/Solt.vue
@@ -18,7 +18,7 @@
             </template> -->
 
             <template #footer>
-                故事都是骗人的111
+                故事都是骗人的111666
             </template>
         </solt-child>
     </div>
